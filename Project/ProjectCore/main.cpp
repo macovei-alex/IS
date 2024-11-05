891d36aab802847bdb38b1e32269cfd185eb8098
macovei-alexandru <maccelmare@gmail.com>
2024-11-05 12:54:12 +0200

    switched from tabs to spaces

--- Project/ProjectCore/main.cpp
+++ Project/ProjectCore/main.cpp
@@ -3,21 +3,21 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
 
-        window.clear(sf::Color::Black);
+		window.clear(sf::Color::Black);
 
-        // window.draw(...);
+		// window.draw(...);
 
-        window.display();
-    }
+		window.display();
+	}
 }
