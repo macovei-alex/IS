338ce1ac291094c51ad407649e7f13cadba6a4d0
macovei-alexandru <maccelmare@gmail.com>
2024-11-05 12:35:47 +0200

    project setup

---
+++ Project/ProjectCore/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+        // window.draw(...);
+
+        window.display();
+    }
+}
