32c48a3479cc4a581cf8c5135ac578286d085be3
macovei-alexandru <maccelmare@gmail.com>
2024-11-19 11:21:21 +0200

    updated events

--- Pacman/PacmanEngine/Game.h
+++ Pacman/PacmanEngine/Game.h
@@ -3,6 +3,7 @@
 #include "IScene.h"
 #include "IWindow.h"
 #include "IGame.h"
+#include "IListener.h"
 #include "Maze.h"
 #include "GameplaySettings.h"
 
@@ -11,13 +12,16 @@
 
 namespace pac
 {
-	class Game : public IGame
+	class Game : public IGame, public IListener
 	{
 	public:
 		Game(std::shared_ptr<IWindow> window, Maze&& maze, const GameplaySettings& settings);
 		void AddScene(std::shared_ptr<IScene> scene);
 		void Run() override;
 		std::shared_ptr<IScene> GetCurrentScene() const;
+
+		void OnEvent(std::shared_ptr<IEvent> event) override;
+
 	private:
 		std::shared_ptr<IWindow> mWindow;
 		std::vector<std::shared_ptr<IScene>> mScenes;
